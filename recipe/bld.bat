d714bf4e9c29e86a587789af37bbca73e29498cf
H. Vetinari <h.vetinari@gmx.com>
2026-07-22 06:56:53 +1100

    switch to stable abi

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,6 @@ mkdir build
 cd build
 
 echo %PATH%
-set PY_VER_NO_DOT=%PY_VER:.=%
 
 set "PREFIX_CYG=%PREFIX:\=/%"
 
@@ -18,11 +17,11 @@ cmake -G "Ninja" ^
     -DLLVM_INCLUDE_DOCS=OFF ^
     -DLLVM_TARGETS_TO_BUILD=X86 ^
     -DLLDB_ENABLE_PYTHON=ON ^
-    -DLLDB_ENABLE_PYTHON_LIMITED_API=OFF ^
+    -DLLDB_ENABLE_PYTHON_LIMITED_API=ON ^
     -DLLDB_ENABLE_SWIG=ON ^
     -DLLDB_PYTHON_RELATIVE_PATH:PATH="..\Lib\site-packages" ^
     -DLLDB_EMBED_PYTHON_HOME=OFF ^
-    -DPython3_LIBRARIES:FILEPATH="%PREFIX_CYG%/libs/python%PY_VER_NO_DOT%.lib" ^
+    -DPython3_LIBRARIES:FILEPATH="%PREFIX_CYG%/libs/python3.lib" ^
     -DPython3_INCLUDE_DIRS:PATH=%PREFIX_CYG%/include ^
     -DPython3_EXECUTABLE:FILEPATH=%PREFIX_CYG%/python.exe ^
     -DSWIG_EXECUTABLE=%LIBRARY_BIN%/swig.exe ^
