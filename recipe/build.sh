2569350369182b98255c78168efe69e673e54b94
Isuru Fernando <isuruf@gmail.com>
2026-09-08 10:50:24 -0700

    Don't drop libpython3.so as unneeded

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,6 +18,10 @@ if [[ "$target_platform" == osx* ]]; then
   export CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
 fi
 
+if [[ "$target_platform" == linux* ]]; then
+  export LDFLAGS="$LDFLAGS -Wl,--no-as-needed"
+fi
+
 if [[ "${target_platform}" != "${build_platform}" ]]; then
   NATIVE_FLAGS="-DCMAKE_C_COMPILER=$CC_FOR_BUILD;-DCMAKE_CXX_COMPILER=$CXX_FOR_BUILD;-DCMAKE_C_FLAGS=-O2;-DCMAKE_CXX_FLAGS=-O2"
   NATIVE_FLAGS="${NATIVE_FLAGS};-DCMAKE_EXE_LINKER_FLAGS=;-DCMAKE_MODULE_LINKER_FLAGS=;-DCMAKE_SHARED_LINKER_FLAGS="
